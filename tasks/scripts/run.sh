e46a1699add30198de16fabbcf84e571ac8207a3
Carlton Stedman <carlton.stedman@gmail.com>
2015-08-26 23:56:16 -0400

    only use pacman when arch, so support in future ubuntu

--- tasks/scripts/run.sh
+++ tasks/scripts/run.sh
@@ -2,15 +2,25 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# update pacman cache
-sudo pacman -Sy
+# arch linux
+if echo $(uname -r) | grep -q ARCH; then
+    # update pacman cache and packages
+    sudo pacman -Syu --noconfirm
 
-# install python2 to use with ansible
-sudo pacman -S --noconfirm python2
-sudo pacman -S --noconfirm python2-pip
+    # install python2 to use with ansible
+    sudo pacman -S --noconfirm python2
+    sudo pacman -S --noconfirm python2-pip
 
-# install ansible and python packages
-sudo pip2 install -r requirements.txt
+    # install ansible and python packages
+    sudo pip2 install -r requirements.txt
+# otherwise, presume debian/ubuntu, for now
+else
+    # update apt cache
+    sudo apt-get update
+
+    # install ansible and python packages
+    sudo pip install -r requirements.txt
+fi
 
 # run the playbook
 sudo ansible-playbook setup.yml -i HOSTS -b -M ./ansible_modules -u cstedman "$@"
